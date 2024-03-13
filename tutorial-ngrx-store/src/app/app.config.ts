d0b2db85d58278a0231c851cb9331997da184123
Kaleb David Chara Toala <desarrollo.15@torresytorres.com>
2024-03-13 17:32:04 -0500

    Creacion e integracion del componente MyCounter

--- tutorial-ngrx-store/src/app/app.config.ts
+++ tutorial-ngrx-store/src/app/app.config.ts
@@ -1,9 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideStore } from '@ngrx/store';
+import { StoreModule, provideStore } from '@ngrx/store';
+import { counterReducer } from './my-counter/counter.reducer';
+
+const modulesAppConfig = importProvidersFrom(
+  StoreModule.forRoot({ count: counterReducer })
+);
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideStore()]
+  providers: [provideRouter(routes), provideStore(),modulesAppConfig],
 };
+
